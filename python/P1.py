770f281440e918863f74f55d6011975370023982
Ishaan Rastogi <97788132+TridentifyIshaan@users.noreply.github.com>
2026-04-14 12:32:54 +0000

    Revising Basics

---
+++ python/P1.py
@@ -0,0 +1,30 @@
+print("Hello, World!")
+
+# Variables and Data Types
+name = "Alice"  # String
+age = 30        # Integer
+height = 5.5    # Float
+is_student = True  # Boolean
+x = None       # NoneType (represents the absence of a value)
+
+# Output the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Case Sensitivity
+myVariable = "This is a variable"
+MyVariable = "This is another variable"
+
+'''
+Variable name rules
+1. Variable names must start with a letter (a-z, A-Z) or an underscore (_)
+2. Variable names can contain letters, digits (0-9), and underscores (_)
+3. Variable names cannot start with a digit
+4. Variable names cannot contain spaces
+5. Variable names cannot be a reserved keyword in Python
+'''
+
+# Convention in every language is to use camelCase for variable names
+myVariableName = "This is a variable name in camelCase"
