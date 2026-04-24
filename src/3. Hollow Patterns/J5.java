312242686086676c388f8995d627d6ef500ae8b2
TridentifyIshaan <ishaanrastogi19@gmail.com>
2026-04-24 19:53:39 +0530

    Hollow Diamond

---
+++ src/3. Hollow Patterns/J5.java
@@ -0,0 +1,45 @@
+/*
+> Date Created: 
+> Author: Ishaan Rastogi
+> Purpose: To print a hollow diamond pattern (pyramid + inverse pyramid)
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+*/
+
+import java.util.*;
+
+class J5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows in the pyramid: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1 || i == 1) { // Here, i == last will be empty
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        // Inverted pyramid has 1 row less than the above rows => n - 1
+        for (int i = 1; i <= n - 1; i++) { // n - 1 = n - 1
+            for (int j = 1; j <= i; j++) { // + 1
+                System.out.print("  ");
+            }
+            // we are -1 twice because for start and end of the loop
+            for (int k = 1; k <= 2 * (n - i) - 1; k++) { // 2 * (n - i) + 1 - 1 - 1 = 2 * (n - i) - 1
+                if (k == 1 || k == 2 * (n - i) - 1 || i == n - 1) { // Here, i == 1 will be empty
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
