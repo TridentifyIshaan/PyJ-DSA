b2f30f5d9d5502644c362bb83d050b15817daebd
TridentifyIshaan <ishaanrastogi19@gmail.com>
2026-06-20 13:54:22 +0530

    Infinite Recursion

---
+++ python/DSA/Recursion/1.py
@@ -0,0 +1,12 @@
+'''
+> Date Created: 20/06/2026
+> Author: Ishaan Rastogi
+> Purpose: To show infinite recursion.
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+'''
+
+def greet():
+    print("Anirudh")
+    greet()
+greet()
